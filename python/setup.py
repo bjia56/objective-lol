fec67663ceb4316aa3a2dbb5ff8b7cdf5da350ed
Brett Jia <dev.bjia56@gmail.com>
2025-09-16 16:34:38 -0400

    bump

--- python/setup.py
+++ python/setup.py
@@ -355,7 +355,7 @@ class CustomBuildExt(build_ext):
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="0.0.1",
+    version="0.0.2",
     author="Brett Jia",
     author_email="dev.bjia56@gmail.com",
     description="Python bindings for Objective-LOL",
